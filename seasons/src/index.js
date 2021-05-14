63abe6beed818d74c2ad3dd377d7b8dcfbe348ae
Bryan Concepcion <bryan@Bryans-iMac.fios-router.home>
2021-05-13 21:27:38 -0400

    submit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null};
+        {/**whenver you want state to be a number eventually
+        set initial state to null */}
+    }
     render(){
         window.navigator.geolocation.getCurrentPosition(
-            (position)=> console.log(position),
+            (position)=> {this.setState({lat: position.coords.latitude})},
             (err)=>console.log(err)
         )
-        return <div>Hi there!</div>;
+        return <div>Latitude:{this.state.lat} </div>;
     }
 }
 
